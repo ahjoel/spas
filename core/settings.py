f58ea817a9c7d006f32c6adbc1734682a43b38af
Joel <aholoujoel5@gmail.com>
2022-09-28 10:34:19 +0000

    Media_root configured

--- core/settings.py
+++ core/settings.py
@@ -113,7 +113,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
