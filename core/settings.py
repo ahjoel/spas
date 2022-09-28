8fd222cc0ad051ad5180b1efea29f7be9b582449
Joel <aholoujoel5@gmail.com>
2022-09-28 09:20:09 +0000

    Settings file configured

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+from decouple import config
+from django.conf.global_settings import DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
