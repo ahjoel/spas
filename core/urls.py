49129baf2f3e6f4a1749ce8bc61f48d65a8f82cd
Joel <aholoujoel5@gmail.com>
2022-09-28 10:22:50 +0000

    Urls file configured

--- core/urls.py
+++ core/urls.py
@@ -13,11 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from . import settings
 
 urlpatterns = [
     path('', include('spas.urls')), # gestion des routes avec l'application
     path('accounts/', include('django.contrib.auth.urls')), # gestion des comptes d'utilisateur
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
